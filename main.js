91238aa755b122b4bf49fc1cffbe8ce0fa5129d2
ahmedFarouk2020 <ahmedfarook0111@gmail.com>
2023-03-26 21:31:45 +0200

    add some features
    reset counter + add new zikr + some nice style

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+btn = document.createElement("button");
+btn.className = "btn";
+btn.textContent = "إضافة ذكر";
+document.body.append(btn);
+
 document.addEventListener( "click" ,function (ev) {
     
     if (ev.target.classList.contains("countable")) {
@@ -9,5 +14,24 @@ document.addEventListener( "click" ,function (ev) {
 
         // increment the counter
         nextSib.textContent = `${++counterVal}`;
+    } else if (ev.target.classList.contains("counter")) {
+        ev.target.textContent = '0';
+    } else if (ev.target.classList.contains("btn")) {
+        // get data in input
+        input = document.getElementsByClassName("input")[0];
+        console.log(input);
+        newZikrData = input.value;
+        console.log(newZikrData)
+        // create new line (counter and zikr field)
+        newZirkElement = document.querySelector(".countable").cloneNode(true);
+        newZirkElement.textContent = newZikrData;
+        newCounterElement = document.querySelector(".counter").cloneNode(true);
+        // add zikr to new line
+        div = document.getElementsByTagName("div")[0];
+        
+        div.appendChild(newZirkElement);
+        
+        div.appendChild(newCounterElement);
     }
 })
+
